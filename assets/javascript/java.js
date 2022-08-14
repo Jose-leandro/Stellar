9858cb42fe133496d4bcc33fd59990598a859e83
jose <lenadrojose124leandro@gmail.com>
2022-08-14 20:47:16 -0300

    terminado a seção categoria e criando as paginas dos binocúlos

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,10 +1,22 @@
+
 function TornarVisivelOMenuAdicionar() {
     var TresPotinhos = document.querySelector("#tres-pontinhos");
-    TresPotinhos.addEventListener("click", function(event) {
+   TresPotinhos.addEventListener("click", function(event) {
         console.log(event)
-        var ContenerAdicionar = document.querySelector(".contener-adicionar");
-        ContenerAdicionar.style.visibility ="visible";
+         var ContenerAdicionar = document.querySelector(".contener-adicionar");
+       ContenerAdicionar.style.visibility = "visible";
     });
 }
 
 TornarVisivelOMenuAdicionar();
+
+function esconderOMenuAdicionar() {
+    var fechar = document.querySelector(".fechar-menu-produtos");
+    fechar.addEventListener("click", function(event) {
+        console.log(event);
+        
+        var ContenerAdicionar = document.querySelector(".contener-adicionar");
+        ContenerAdicionar.style.visibility = "hidden";
+    });
+}esconderOMenuAdicionar();
+
