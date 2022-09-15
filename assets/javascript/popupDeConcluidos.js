f32bf135bbc176c6288f0753818f2d825747740a
jose <lenadrojose124leandro@gmail.com>
2022-09-14 21:23:07 -0300

    melhorei o arquivo adicionarProdutos , binoculos e comercei binoculoPretoDiamonback

--- assets/javascript/popupDeConcluidos.js
+++ assets/javascript/popupDeConcluidos.js
@@ -1,21 +1,21 @@
 
 function mostarPopup() {
 
-    var contenerPopup = document.querySelector(".contener-popup");
+    var contenerPopup = document.querySelector(".contener__popup");
     contenerPopup.style.visibility = "visible";
 }
 
 function fecharPopup() {
-    var contnerPopup = document.querySelector(".contener-popup");
+    var contnerPopup = document.querySelector(".contener__popup");
 
-    var popupClose = document.querySelector(".popup-close");
+    var popupClose = document.querySelector(".popup__close");
     popupClose.addEventListener("click", function () {
-        var contenerPopup = document.querySelector(".contener-popup");
+        var contenerPopup = document.querySelector(".contener__popup");
         contenerPopup.style.visibility = "hidden";
     });
 
     contnerPopup.addEventListener("click", function () {
-        var contenerPopup = document.querySelector(".contener-popup");
+        var contenerPopup = document.querySelector(".contener__popup");
         contenerPopup.style.visibility = "hidden";
     });
 } fecharPopup()
