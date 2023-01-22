4dfa84afaffe483401e02f319d94c25539e2b1a8
jose <lenadrojose124leandro@gmail.com>
2023-01-22 16:59:45 -0300

    Resolvendo bags, melhorando o css e seguindo para os últimos passos para a conclusão do projeto

--- admin/assets/javascript/teclaEnter.js
+++
@@ -1,7 +0,0 @@
-
-inputBuscar.addEventListener("keydown", (event) => {
-    console.log(event.key)
-    if (event.key == "Enter") {
-        window.location.href = "pesquisarProdutos.html"
-    }
-})
