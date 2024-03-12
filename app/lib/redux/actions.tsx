f0dc259edfd2a87d5eed2b67d69efbb8cb7bced4
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-03-12 12:40:21 -0300

    feat: gerenciamento de estado com Redux

---
+++ app/lib/redux/actions.tsx
@@ -0,0 +1,15 @@
+export const setQualEstadoUsuario = (value: boolean) => ({
+  type: 'SET_QUAL_ESTADO_USUARIO',
+  payload: value
+})
+
+export const setEstaValidadoCampos = (value: boolean) => ({
+  type: 'SET_ESTA_VALIDADO_CAMPOS',
+  payload: value
+})
+
+export const setQualNomeUsuario = (value: string) => ({
+  type: 'SET_QUAL_NOME_USUARIO',
+  payload: value
+})
+
