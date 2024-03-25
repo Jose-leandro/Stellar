3090661f0b0afe72434dc33a20e1c27010ede360
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-03-25 09:29:59 -0300

    feat: Início do desenvolvimento das páginas de produtos disponíveis para venda

---
+++ pages/produtos/binoculo/fpeheh.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Header from '../../../app/components/header'
+import Footer from '../../../app/components/footer'
+
+const propsCradBinoculosFpeheh = {
+    produtoAVenda: {
+        nome: 'Fpeheh',
+        preco: 'R$ 800',
+        imgProdutoAVenda: '/img/binoculo-produto-3.jpg'
+    },
+    produtoSimilar1: {
+        nome: 'BcPel',
+        preco: 'R$ 460',
+        imgProdutoAVenda: '/img/binoculos-produto-1.jpg',
+        rotaProduto: '/produtos/binoculo/bcpel'
+    },
+    produtoSimilar2: {
+        nome: 'BlueGlass',
+        preco: 'R$ 550',
+        imgProdutoAVenda: '/img/binoculos-produto-1.jpg',
+        rotaProduto: '/produtos/binoculo/backbate'
+    },
+    produtoSimilar3: {
+        nome: 'Diamondba',
+        preco: 'R$ 300',
+        imgProdutoAVenda: '/img/binoculo-produto-4.jpg',
+        rotaProduto: '/produtos/binoculo/diamondba'
+    }
+}
+
+export default function fpeheh(): React.JSX.Element {
+    return (
+        <>
+            <header>
+                <Header />
+            </header>
+
+            <main>
+            <CardBinoculo {...propsCradBinoculosFpeheh} />
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
+        </>
+    )
+}
