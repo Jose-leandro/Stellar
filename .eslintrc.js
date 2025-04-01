9125367df2f8f5976faa91e21d8df255823ab9b9
Jose-leandro <lenadrojose124leandro@gmail.com>
2025-04-01 17:16:01 -0300

    chore: update ESLint config for Prettier, Next.js, and more

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     "extends": [
         "plugin:react/recommended",
         "standard-with-typescript",
-        "prettier"
+        "prettier",
+        "eslint:recommended",
+        "next/core-web-vitals"
     ],
     "overrides": [
     ],
@@ -25,5 +27,8 @@ module.exports = {
     },
     "rules": {
         'prettier/prettier': 'error',
+        'no-eval': 'error',               // Prevents using eval()
+        'no-implied-eval': 'error',       // Prevents using strings in setTimeout() and setInterval()
+        'no-new-func': 'error',           // Prevents using new Function() constructor
     }
 }
