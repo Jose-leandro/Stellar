0e08a56cb7367c0dc2df2ab6373fb178566ce625
Jose-leandro <lenadrojose124leandro@gmail.com>
2025-04-01 17:34:08 -0300

    feat: start developing new search feature for Strellar

--- app/components/main/main.tsx
+++ app/components/main/main.tsx
@@ -3,10 +3,73 @@ import { CategoriasPopulares } from './categoriasPopulares'
 import { BannerInicial } from './bannerInicial'
 import { CriarProdutos } from './CriarProdutos'
 
-export function Main (): React.JSX.Element {
+export function Main(): React.JSX.Element {
+  const amountOfItems = "200"
+  const titleCategori = "arroz"
+  const titleProduct = "feliz"
+  const valueProduct = 200
+
   return (
     <>
-      <BannerInicial />
+
+      <section>
+
+        <img src='public/img/nature.jpg' />
+        <h1>Your Gateway to Infinite Possibilities</h1>
+        <h2>From stargazing equipment to everyday treasures</h2>
+        <h3>Join our global community of entrepreneurs, collectors, and sellers</h3>
+
+        <input placeholder='Search for anything under the strs...' />
+        <img src="" />
+
+      </section>
+
+      <section>
+        <h2>Explore Categories</h2>
+        <span>From telescopes to treasure, find exactly what you are looking for</span>
+
+        {
+          <div>
+            <img />
+            <div>
+              <h2>Astronomy & Space</h2>
+              <h2> {amountOfItems}+ items </h2>
+            </div>
+          </div>
+        }
+
+      </section>
+
+      <section>
+
+        <div>
+          <h2>Featured Prosucts</h2>
+          <span>Discover our most popular items</span>
+        </div>
+
+        <div>
+          {/* "use radix ui for creater" */}
+        </div>
+
+        {
+          <div>
+            <img />
+            <h3> {titleCategori} </h3>
+
+            <div>
+              <div>
+                <h3> {titleProduct} </h3>
+                <span> {valueProduct} </span>
+              </div>
+
+            </div>
+          </div>
+        }
+
+      </section>
+
+
+      {/* <BannerInicial />
       <CategoriasPopulares />
       <CriarProdutos tipoCategoria={'Refratores'} tipoProduto={'refratores'} />
       <CriarProdutos tipoCategoria={'Binoculos'} tipoProduto={'binoculos'} />
@@ -14,7 +77,7 @@ export function Main (): React.JSX.Element {
       <CriarProdutos
         tipoCategoria={'Fotos Astrônomicas'}
         tipoProduto={'fotosAstronomicas'}
-      />
+      /> */}
     </>
   )
 }
