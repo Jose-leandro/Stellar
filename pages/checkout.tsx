2922decc091384e8eb87dc4418f19b17e7cad2b4
josé leandro do nascimento <104599482+Jose-leandro@users.noreply.github.com>
2025-04-08 20:19:17 +0000

    style(checkout): refine styles and align layout closer to Stellar design

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,36 +1,84 @@
 import React from 'react'
+import Footer from '../app/components/footer'
+import Header from '../app/components/header'
+import Head from 'next/head'
+import Image from 'next/image'
+import DOMPurify from 'isomorphic-dompurify';
+
+const decreaseQuantity = () => {
+
+}
+
+const increaseQuantity = () => {
+
+}
 
 export default function Checkout() {
+    const subtotalValule = "R$ 40"
+    const ShippingValue = "R$ 1"
+    const taxValule = "R$ 2"
+    const totalValule = "R$ 100"
+
+    const products = [
+        {
+          id: 1,
+          name: "Tênis Esportivo",
+          price: 199.99,
+          quantity: 2,
+          imageUrl: "/img/binoculo-produto-2.jpg"
+        },
+        {
+          id: 2,
+          name: "Camiseta Básica",
+          price: 79.90,
+          quantity: 1,
+          imageUrl: "/img/binoculo-produto-2.jpg"
+        }
+      ];
+      
+      
     return (
         <>
-            <h1 classname="font-bold size-5 " >Checkout</h1>
+        <Head>
+        <title>Checkout - Stellar</title>
+        <meta name="description" content="" />
+      </Head>
+      <Header />
+
+      <main>
+        <section className='flex flex-col items-center justify-center w-full'>
+            <h1 className="font-bold text-2xl w-1/6 mb-3" >Checkout</h1>
             <div>
-                <h2>
+                <h2 className='text-xl mb-3'>
                     Your Cart
                 </h2>
 
-                {
-                    <div>
-                        <img />
+                {products.map((product, index) => (
+                    <div key={index} className='flex justify-around mb-6 mt-2'>
+                        <Image src={product.imageUrl} alt={product.name} height={100} width={100} />
 
-                        <div>
-                            <h3>Products 1</h3>
-                            <span>R$ 200</span>
+                        <div className='flex-col justify-around'>
+                        <h3 className='text-xl'>{product.name}</h3>
+                        <span className='mt-1'>R$ {product.price.toFixed(2)}</span>
 
-                            <div>
-                                <button>
-                                    <img></img>
-                                </button>
+                        <div className='flex justify-around items-center mt-4'>
+                            <button className='bg-slate-500 w-6 h-6 rounded-sm' onClick={() => decreaseQuantity(product.id)}>
+                            -
+                            </button>
 
-                                <span>1</span>
+                            <span>{product.quantity}</span>
 
-                                <button>
-                                    <img></img>
-                                </button>
-                            </div>
+                            <button className='w-6 bg-slate-700 h-6' onClick={() => increaseQuantity(product.id)}>
+                            +
+                            </button>
                         </div>
+                        </div>
+
+                        <button className='ml-3 text-1xl'>
+                            X
+                        </button>
                     </div>
-                }
+                ))}
 
                 <div>
                     <h4>
@@ -112,6 +160,9 @@ export default function Checkout() {
                 </form>
 
             </div>
-        </>
+        </section>
+        </main>
+    <Footer />
+    </>
     )
 }
